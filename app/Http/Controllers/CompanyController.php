d7277c8b5dfbc34f87a067fcfea41ad720ce0978
jannydream0826 <michaelgovern0826@gmail.com>
2023-10-17 04:33:23 +0900

    api documentation

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -5,90 +5,298 @@ namespace App\Http\Controllers;
 use App\Models\Company;
 use App\Models\Station;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
+/**
+ * @OA\Info(
+ *     title="Virta API",
+ *     version="1.0",
+ *     description="API for managing companies and their stations.",
+ *     @OA\Contact(
+ *         email="michaelgovern0826@gmail.com",
+ *         name="Michael Govern"
+ *     )
+ * )
+ */
 class CompanyController extends Controller
 {
     /**
      * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     *
+     * @OA\Get(
+     *     path="/api/company",
+     *     summary="Get a list of companies",
+     *     description="Returns a list of all companies.",
+     *     @OA\Response(
+     *         response=200,
+     *         description="Successful operation",
+     *         @OA\JsonContent(
+     *             type="array",
+     *             @OA\Items(
+     *                 type="object",
+     *                 properties={
+     *                     @OA\Property(property="id", type="integer"),
+     *                     @OA\Property(property="parent_company_id", type="integer"),
+     *                     @OA\Property(property="name", type="string"),
+     *                 }
+     *             )
+     *         )
+     *     )
+     * )
      */
     public function index()
     {
         $companies = Company::all();
-        return response()->json($companies);
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-
+        return response()->json($companies, 200);
     }
 
     /**
      * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     *
+     * @OA\Post(
+     *     path="/api/company",
+     *     summary="Create a new company",
+     *     description="Create a new company record.",
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(
+     *             type="object",
+     *             properties={
+     *                 @OA\Property(property="parent_company_id", type="integer"),
+     *                 @OA\Property(property="name", type="string"),
+     *             }
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=201,
+     *         description="Company created successfully",
+     *         @OA\JsonContent(
+     *             type="object",
+     *             properties={
+     *                 @OA\Property(property="id", type="integer"),
+     *                 @OA\Property(property="parent_company_id", type="integer"),
+     *                 @OA\Property(property="name", type="string"),
+     *             }
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=400,
+     *         description="Bad request"
+     *     )
+     * )
      */
     public function store(Request $request)
     {
+        $request->validate([
+            'parent_company_id' => 'nullable|integer',
+            'name' => 'required|string|min:5',
+        ], [
+            'name.required' => 'The name field is required.',
+        ]);
+
         try {
             $company = Company::create($request->all());
-            $company->parent_company_id = $request->parentId;
             return response()->json($company, 201);
         } catch (\Throwable $th) {
-            return response()->json($th);
+            return response()->json(['error' => 'Failed to create the company.'], 400);
         }
     }
 
     /**
      * Display the specified resource.
+     *
+     * @param  \App\Models\Company  $company
+     * @return \Illuminate\Http\Response
+     *
+     * @OA\Get(
+     *     path="/api/company/{company}",
+     *     summary="Get a specific company",
+     *     description="Returns details of a specific company.",
+     *     @OA\Parameter(
+     *         name="company",
+     *         in="path",
+     *         description="ID of the company to retrieve",
+     *         required=true,
+     *         @OA\Schema(type="integer")
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Successful operation",
+     *         @OA\JsonContent(type="object",
+     *             properties={
+     *                 @OA\Property(property="id", type="integer"),
+     *                 @OA\Property(property="parent_company_id", type="integer"),
+     *                 @OA\Property(property="name", type="string"),
+     *             })
+     *     ),
+     *     @OA\Response(
+     *         response=404,
+     *         description="Company not found"
+     *     )
+     * )
      */
     public function show(Company $company)
     {
-        return response()->json($company);
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
+        if ($company) {
+            return response()->json($company, 200);
+        } else {
+            return response()->json(['error' => 'Company not found.'], 404);
+        }
     }
 
     /**
      * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Company  $company
+     * @return \Illuminate\Http\Response
+     *
+     * @OA\Put(
+     *     path="/api/company/{company}",
+     *     summary="Update a company",
+     *     description="Update a company record.",
+     *     @OA\Parameter(
+     *         name="company",
+     *         in="path",
+     *         description="ID of the company to update",
+     *         required=true,
+     *         @OA\Schema(type="integer")
+     *     ),
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(
+     *             type="object",
+     *             properties={
+     *                 @OA\Property(property="name", type="string"),
+     *             }
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Company updated successfully",
+     *         @OA\JsonContent(type="object",
+     *             properties={
+     *                 @OA\Property(property="id", type="integer"),
+     *                 @OA\Property(property="parent_company_id", type="integer"),
+     *                 @OA\Property(property="name", type="string"),
+     *             })
+     *     ),
+     *     @OA\Response(
+     *         response=400,
+     *         description="Bad request"
+     *     )
+     * )
      */
     public function update(Request $request, Company $company)
     {
         $request->validate([
             'name' => 'required|string|min:5',
         ], [
-            'name.required' => 'This name is required',
+            'name.required' => 'The name field is required.',
         ]);
 
-        $company->name = $request->name;
-        $company->parent_company_id = $request->parentId;
-        $company->save();
-        return response()->json($company, 200);
+        try {
+            $company->update($request->all());
+            return response()->json($company, 200);
+        } catch (\Throwable $th) {
+            return response()->json(['error' => 'Failed to update the company.'], 400);
+        }
     }
 
     /**
      * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Company  $company
+     * @return \Illuminate\Http\Response
+     *
+     * @OA\Delete(
+     *     path="/api/company/{company}",
+     *     summary="Delete a company",
+     *     description="Delete a company record.",
+     *     @OA\Parameter(
+     *         name="company",
+     *         in="path",
+     *         description="ID of the company to delete",
+     *         required=true,
+     *         @OA\Schema(type="integer")
+     *     ),
+     *     @OA\Response(
+     *         response=204,
+     *         description="Company deleted successfully"
+     *     ),
+     *     @OA\Response(
+     *         response=404,
+     *         description="Company not found"
+     *     )
+     * )
      */
-    public function destroy(string $id)
+    public function destroy(Company $company)
     {
-        Company::findOrFail($id)->delete();
-        return response()->json(null, 204);
+        try {
+            $company->delete();
+            return response()->json(null, 204);
+        } catch (\Throwable $th) {
+            return response()->json(['error' => 'Failed to delete the company.'], 400);
+        }
     }
 
+    /**
+     * Get stations owned by a company and its children.
+     *
+     * @param  int  $company_id
+     * @return \Illuminate\Http\Response
+     *
+     * @OA\Get(
+     *     path="/api/child-stations/{company_id}",
+     *     summary="Get stations owned by a company and its children",
+     *     description="Returns a list of stations owned by a company and its children.",
+     *     @OA\Parameter(
+     *         name="company_id",
+     *         in="path",
+     *         description="ID of the company to retrieve stations for",
+     *         required=true,
+     *         @OA\Schema(type="integer")
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Successful operation",
+     *         @OA\JsonContent(
+     *             type="array",
+     *             @OA\Items(
+     *                 type="object",
+     *                 properties={
+     *                     @OA\Property(property="id", type="integer"),
+     *                     @OA\Property(property="name", type="string"),
+     *                     @OA\Property(property="latitude", type="number", format="float"),
+     *                     @OA\Property(property="longitude", type="number", format="float"),
+     *                     @OA\Property(property="company_id", type="integer"),
+     *                     @OA\Property(property="address", type="string"),
+     *                 })
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=404,
+     *         description="Company not found"
+     *     )
+     * )
+     */
     public function childStations($company_id)
     {
         $company = Company::findOrFail($company_id);
-        // Get all child companies recursively
-        $childCompanies = $company->getChildCompanies();
-        // Get all stations owned by child companies and the current company
-        $stations = Station::whereIn('company_id', $childCompanies->pluck('id')->push($company_id))->get();
+        if ($company) {
+            // Get all child companies recursively
+            $childCompanies = $company->getChildCompanies();
+            // Get all stations owned by child companies and the current company
+            $stations = Station::whereIn('company_id', $childCompanies->pluck('id')->push($company_id))->get();
 
-        return response()->json($stations);
+            return response()->json($stations, 200);
+        } else {
+            return response()->json(['error' => 'Company not found.'], 404);
+        }
     }
 }
--- app/Http/Controllers/StationController.php
+++ app/Http/Controllers/StationController.php
@@ -10,52 +10,223 @@ class StationController extends Controller
 {
     /**
      * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     *
+     * @OA\Get(
+     *     path="/api/station",
+     *     summary="Get a list of charging stations",
+     *     description="Returns a list of all charging stations.",
+     *     @OA\Response(
+     *         response=200,
+     *         description="Successful operation",
+     *         @OA\JsonContent(
+     *             type="array",
+     *             @OA\Items(
+     *                 type="object",
+     *                 properties={
+     *                     @OA\Property(property="id", type="integer"),
+     *                     @OA\Property(property="name", type="string"),
+     *                     @OA\Property(property="latitude", type="number", format="float"),
+     *                     @OA\Property(property="longitude", type="number", format="float"),
+     *                     @OA\Property(property="company_id", type="integer"),
+     *                     @OA\Property(property="address", type="string"),
+     *                 }
+     *             )
+     *         )
+     *     )
+     * )
      */
     public function index()
     {
         $stations = Station::all();
-        return response()->json($stations);
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
+        return response()->json($stations, 200);
     }
 
     /**
      * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     *
+     * @OA\Post(
+     *     path="/api/station",
+     *     summary="Create a new charging station",
+     *     description="Create a new charging station record.",
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(
+     *             type="object",
+     *             properties={
+     *                 @OA\Property(property="name", type="string"),
+     *                 @OA\Property(property="latitude", type="number", format="float"),
+     *                 @OA\Property(property="longitude", type="number", format="float"),
+     *                 @OA\Property(property="company_id", type="integer"),
+     *                 @OA\Property(property="address", type="string"),
+     *             }
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=201,
+     *         description="Station created successfully",
+     *         @OA\JsonContent(
+     *             type="object",
+     *             properties={
+     *                 @OA\Property(property="id", type="integer"),
+     *                 @OA\Property(property="name", type="string"),
+     *                 @OA\Property(property="latitude", type="number", format="float"),
+     *                 @OA\Property(property="longitude", type="number", format="float"),
+     *                 @OA\Property(property="company_id", type="integer"),
+     *                 @OA\Property(property="address", type="string"),
+     *             }
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=400,
+     *         description="Bad request"
+     *     )
+     * )
      */
     public function store(Request $request)
     {
+        $request->validate([
+            'name' => 'required|string',
+            'latitude' => 'required|numeric',
+            'longitude' => 'required|numeric',
+            'company_id' => 'required|integer',
+            'address' => 'required|string',
+        ]);
+
         try {
             $station = Station::create($request->all());
             return response()->json($station, 201);
         } catch (\Throwable $th) {
-            return response()->json($th);
+            return response()->json(['error' => 'Failed to create the station.'], 400);
         }
     }
 
     /**
      * Display the specified resource.
+     *
+     * @param  \App\Models\Station  $station
+     * @return \Illuminate\Http\Response
+     *
+     * @OA\Get(
+     *     path="/api/station/{station}",
+     *     summary="Get a specific charging station",
+     *     description="Returns details of a specific charging station.",
+     *     @OA\Parameter(
+     *         name="station",
+     *         in="path",
+     *         description="ID of the station to retrieve",
+     *         required=true,
+     *         @OA\Schema(type="integer")
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Successful operation",
+     *         @OA\JsonContent(
+     *             type="object",
+     *             properties={
+     *                 @OA\Property(property="id", type="integer"),
+     *                 @OA\Property(property="name", type="string"),
+     *                 @OA\Property(property="latitude", type="number", format="float"),
+     *                 @OA\Property(property="longitude", type="number", format="float"),
+     *                 @OA\Property(property="company_id", type="integer"),
+     *                 @OA\Property(property="address", type="string"),
+     *             }
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=404,
+     *         description="Station not found"
+     *     )
+     * )
      */
     public function show(Station $station)
     {
-        return response()->json($station);
+        if ($station) {
+            return response()->json($station, 200);
+        } else {
+            return response()->json(['error' => 'Station not found.'], 404);
+        }
     }
-
     /**
-     * Show the form for editing the specified resource.
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Station  $station
+     * @return \Illuminate\Http\Response
+     *
+     * @OA\Delete(
+     *     path="/api/station/{station}",
+     *     summary="Delete a charging station",
+     *     description="Delete a charging station record.",
+     *     @OA\Parameter(
+     *         name="station",
+     *         in="path",
+     *         description="ID of the station to delete",
+     *         required=true,
+     *         @OA\Schema(type="integer")
+     *     ),
+     *     @OA\Response(
+     *         response=204,
+     *         description="Station deleted successfully"
+     *     ),
+     *     @OA\Response(
+     *         response=404,
+     *         description="Station not found"
+     *     )
+     * )
      */
-    public function edit(string $id)
-    {
-        //
-    }
-
     /**
      * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Station  $station
+     * @return \Illuminate\Http\Response
+     *
+     * @OA\Put(
+     *     path="/api/station/{station}",
+     *     summary="Update a charging station",
+     *     description="Update a charging station record.",
+     *     @OA\Parameter(
+     *         name="station",
+     *         in="path",
+     *         description="ID of the station to update",
+     *         required=true,
+     *         @OA\Schema(type="integer")
+     *     ),
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(
+     *             type="object",
+     *             properties={
+     *                 @OA\Property(property="name", type="string"),
+     *                 @OA\Property(property="latitude", type="number", format="float"),
+     *                 @OA\Property(property="longitude", type="number", format="float"),
+     *                 @OA\Property(property="company_id", type="integer"),
+     *                 @OA\Property(property="address", type="string"),
+     *             }
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Station updated successfully",
+     *         @OA\JsonContent(type="object",
+     *             properties={
+     *                 @OA\Property(property="id", type="integer"),
+     *                 @OA\Property(property="name", type="string"),
+     *                 @OA\Property(property="latitude", type="number", format="float"),
+     *                 @OA\Property(property="longitude", type="number", format="float"),
+     *                 @OA\Property(property="company_id", type="integer"),
+     *                 @OA\Property(property="address", type="string"),
+     *             })
+     *     ),
+     *     @OA\Response(
+     *         response=400,
+     *         description="Bad request"
+     *     )
+     * )
      */
     public function update(Request $request, Station $station)
     {
@@ -73,39 +244,112 @@ class StationController extends Controller
             'address.required' => 'This address is required',
         ]);
 
-        $station->name = $request->name;
-        $station->latitude = $request->latitude;
-        $station->longitude = $request->longitude;
-        $station->company_id = $request->company_id;
-        $station->address = $request->address;
-        $station->save();
-        return response()->json($station, 200);
+        try {
+            $station->update($request->all());
+            return response()->json($station, 200);
+        } catch (\Throwable $th) {
+            return response()->json(['error' => 'Failed to update the station.'], 400);
+        }
     }
-
     /**
      * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Station  $station
+     * @return \Illuminate\Http\Response
+     *
+     * @OA\Delete(
+     *     path="/api/station/{station}",
+     *     summary="Delete a charging station",
+     *     description="Delete a charging station record.",
+     *     @OA\Parameter(
+     *         name="station",
+     *         in="path",
+     *         description="ID of the station to delete",
+     *         required=true,
+     *         @OA\Schema(type="integer")
+     *     ),
+     *     @OA\Response(
+     *         response=204,
+     *         description="Station deleted successfully"
+     *     ),
+     *     @OA\Response(
+     *         response=404,
+     *         description="Station not found"
+     *     )
+     * )
      */
-    public function destroy(string $id)
+    public function destroy(Station $station)
     {
-        Station::findOrFail($id)->delete();
-        return response()->json(null, 204);
+        try {
+            $station->delete();
+            return response()->json(null, 204);
+        } catch (\Throwable $th) {
+            return response()->json(['error' => 'Failed to delete the station.'], 400);
+        }
     }
-
+    /**
+     * Get charging stations within a specified radius.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     *
+     * @OA\Get(
+     *     path="/api/stations-within-radius",
+     *     summary="Get charging stations within a radius",
+     *     description="Returns a list of charging stations within a specified radius.",
+     *     @OA\Parameter(
+     *         name="latitude",
+     *         in="query",
+     *         description="Latitude",
+     *         required=true,
+     *         @OA\Schema(type="number", format="float")
+     *     ),
+     *     @OA\Parameter(
+     *         name="longitude",
+     *         in="query",
+     *         description="Longitude",
+     *         required=true,
+     *         @OA\Schema(type="number", format="float")
+     *     ),
+     *     @OA\Parameter(
+     *         name="radius",
+     *         in="query",
+     *         description="Radius (in kilometers)",
+     *         required=true,
+     *         @OA\Schema(type="number", format="float")
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Successful operation",
+     *         @OA\JsonContent(type="object")
+     *     ),
+     *     @OA\Response(
+     *         response=400,
+     *         description="Bad request"
+     *     )
+     * )
+     */
     public function stationsWithinRadius(Request $request)
     {
         $latitude = $request->query('latitude');
         $longitude = $request->query('longitude');
         $radius = $request->query('radius');
-        $stations = DB::table('stations')
-            ->selectRaw('stations.*, 
-            (6371 * acos(cos(radians(?)) * cos(radians(latitude)) * cos(radians(longitude) - radians(?)) + sin(radians(?)) * sin(radians(latitude)))) AS distance',
-                [$latitude, $longitude, $latitude]
-            )
-            ->having('distance', '<=', $radius)
-            ->orderBy('distance')
-            ->get();
-        // Group stations by location (latitude and longitude)
-        $groupedStations = $stations->groupBy(['latitude', 'longitude']);
-        return response()->json($groupedStations);
+
+        try {
+            $stations = DB::table('stations')
+                ->selectRaw('stations.*, 
+                (6371 * acos(cos(radians(?)) * cos(radians(latitude)) * cos(radians(longitude) - radians(?)) + sin(radians(?)) * sin(radians(latitude)))) AS distance',
+                    [$latitude, $longitude, $latitude]
+                )
+                ->having('distance', '<=', $radius)
+                ->orderBy('distance')
+                ->get();
+
+            // Group stations by location (latitude and longitude)
+            $groupedStations = $stations->groupBy(['latitude', 'longitude']);
+            return response()->json($groupedStations, 200);
+        } catch (\Throwable $th) {
+            return response()->json(['error' => 'Failed to retrieve stations within the specified radius.'], 400);
+        }
     }
 }
